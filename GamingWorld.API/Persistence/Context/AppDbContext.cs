2d903c84c4bf0361d8d0b0557b4a98538ca8e895
Lossani <dev@xempre.com>
2021-10-31 23:49:24 -0500

    Refactor: Deleted "TypeUser" and fixed typo in User.cs.

--- GamingWorld.API/Persistence/Context/AppDbContext.cs
+++ GamingWorld.API/Persistence/Context/AppDbContext.cs
@@ -19,18 +19,16 @@ namespace GamingWorld.API.Persistence.Context
             builder.Entity<User>().HasKey(p => p.Id);
             builder.Entity<User>().Property(p => p.Id).IsRequired().ValueGeneratedOnAdd();
             builder.Entity<User>().Property(p => p.Username).IsRequired().HasMaxLength(30);
-            builder.Entity<User>().Property(p => p.TypeUser).IsRequired().HasMaxLength(30);
             builder.Entity<User>().Property(p => p.Email).IsRequired().HasMaxLength(50);
             builder.Entity<User>().Property(p => p.Password).IsRequired().HasMaxLength(30);
-            builder.Entity<User>().Property(p => p.Premiun).IsRequired();
+            builder.Entity<User>().Property(p => p.Premium).IsRequired();
 
             builder.Entity<User>().HasData
             (
-                new User { Id = 001, Username = "Fjorpa", TypeUser = "Gamer", Email = "fjorpa@hotmail.com", Password = "123456", Premiun = true},
-                new User { Id = 002, Username = "GoldenX", TypeUser = "Streamer", Email = "goldenx@teyapeo.pe", Password = "helpme", Premiun = false},
-                new User { Id = 003, Username = "Panochita69", TypeUser = "Streamer", Email = "omevengo@yahoo.es", Password = "jaja123", Premiun = false},
-                new User { Id = 004, Username = "RatPalyer", TypeUser = "Gamer", Email = "minecra@gmail.com", Password = "awadecoco", Premiun = true},
-                new User { Id = 005, Username = "Loel76", TypeUser = "Streamer", Email = "loel76@hotmail.es", Password = "xdxdxd", Premiun = false}
+                new User { Id = 1, Username = "Fjorpa", Email = "fjorpa@hotmail.com", Password = "123456", Premium = true},
+                new User { Id = 2, Username = "GoldenX", Email = "goldenx@teyapeo.pe", Password = "helpme", Premium = false},
+                new User { Id = 3, Username = "RatPalyer", Email = "minecra@gmail.com", Password = "awadecoco", Premium = true},
+                new User { Id = 4, Username = "Loel76", Email = "loel76@hotmail.es", Password = "xdxdxd", Premium = false}
             );
         }
     }
